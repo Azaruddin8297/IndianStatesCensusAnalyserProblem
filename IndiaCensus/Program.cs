71c4c716c5af1d757453117ced5b8abc88c1b398
Azaruddin8297 <ajarpopo.com>
2023-04-26 23:24:56 +0530

    India Census

---
+++ IndiaCensus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndiaCensus
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indain Sensus Analyser Preogram");
+        }
+    }
+}
